67d1ec68cd57a357b2bc5fd98d5f385ca77876d2
Stan-sst <staslyakhov@gmail.com>
2017-02-23 23:20:00 -0800

    Added comments for Pellet

--- org.sstctf.snake_game/src/org/sstctf/snake_game/Pellet.java
+++ org.sstctf.snake_game/src/org/sstctf/snake_game/Pellet.java
@@ -7,19 +7,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
+/**
+ * This class represents the pellet that can be picked up by the snake.
+ * The class deals with spawning the pellet
+ * and rendering it.
+ * 
+ * It is an object of the game and thus extends GameObject.
+ * 
+ * @author Andrew Quach
+ * @author Stanislav Lyakhov
+ *
+ * @version 1.0.0
+ */
 public class Pellet extends GameObject {
 
     private Handler handler;
     private boolean isPellet;
 
+    /**
+     * Creates a pellet given its X and Y coordinates.
+     * Sets the ID of the object to Pellet.
+     *  
+     * @param posX the position of the pellet on the X axis
+     * @param posY the position of the pellet on the Y axis
+     * @param handler handles all game objects
+     */
     public Pellet(int posX, int posY, Handler handler) {
         super(posX, posY, ID.Pellet);
         this.handler = handler;
         isPellet = true;
     }
 
-    // Makes sure the pellet spawns in an non occupied area
+
+    /**
+     * Handles the random generation of a pellet object.
+     * 
+     * @return the coordinates of the next spawn point of the pellet
+     */
     public int[] randomSpawn() {
         List<int[]> occupiedSpaces = null;
         for (int i = 0; i < handler.objects.size(); i++) {
@@ -37,20 +61,41 @@ public class Pellet extends GameObject {
         }
         return new int[]{-1, -1};
     }
-    
+
+    /**
+     * Checks if a randomly generated spawn point is occupied.
+     * Helper method for generating random spawn point for the pellet object.
+     * 
+     * @param spawn array containing the X and  Y coordinates of a planned spawn point
+     * @param occupiedSpaces list of cood1inates of spaces occupied by the snake
+     * 
+     * @return true if spawn point is occupied
+     * @return false if spawn point is unoccupied
+     * 
+     */
     private boolean checkSpace(int[] spawn, List<int[]> occupiedSpaces){
-    	for (int i = 0; i < occupiedSpaces.size(); i++) {
-    		if (Arrays.equals(occupiedSpaces.get(i), spawn)) {
-    			return false;
-    		}
-    	}
-    	return true;
+        for (int i = 0; i < occupiedSpaces.size(); i++) {
+            if (Arrays.equals(occupiedSpaces.get(i), spawn)) {
+                return false;
+            }
+        }
+        return true;
     }
-    
+
+    /**
+     * Sets the pellet to either existing/not existing
+     * 
+     * @param isPellet
+     */
     public void setPellet(boolean isPellet) {
         this.isPellet = isPellet;
     }
 
+    /**
+     * Override of GameObject tick method.
+     * Checks if pellet exists every tick,
+     * if not spawns new pellet.
+     */
     @Override
     public void tick() {
         if (!isPellet) {
@@ -59,12 +104,23 @@ public class Pellet extends GameObject {
         }
     }
 
+    /**
+     * Override of GameObject render method.
+     * Handles the rendering of the pellet.
+     * Yellow rectangle.
+     */
     @Override
     public void render(Graphics g) {
         g.setColor(Color.YELLOW);
         g.fillRect(getPosition()[0]*Game.SCALE, getPosition()[1]*Game.SCALE, Game.SCALE, Game.SCALE);
     }
 
+    /**
+     * Override of GameObject getBounds method.
+     * Handles the hitboxes of the pellet.
+     * 
+     * @return the hitboxes of the pellet
+     */
     @Override
     public List<Rectangle> getBounds() {
         List<Rectangle> hitboxes = new ArrayList<Rectangle>();
